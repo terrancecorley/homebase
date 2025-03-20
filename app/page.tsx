9bdc285f2623f164cc718d1ae94297e611cf4d8d
Terrance Corley <terrancecorley@outlook.com>
2025-03-19 21:42:28 -0700

    refactored weather widget, added duck duck go searching

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,74 @@
-'use client';
+"use client";
 
-import { Input } from '@/components/ui/input';
-import { Sun } from 'lucide-react';
-import { useState, useEffect } from 'react';
+import { Input } from "@/components/ui/input";
+import { Sun } from "lucide-react";
+import { useState, useEffect } from "react";
 
 type WeatherCurrent = {
-    apparent_temperature: number;
-}
+    temperature_2m: number;
+};
+
+type WeatherCurrentUnits = {
+    temperature_2m: string;
+};
 
 type WeatherData = {
-    current?: WeatherCurrent;
-}
+    current: WeatherCurrent;
+    current_units: WeatherCurrentUnits;
+};
 
 async function read_weather(): Promise<WeatherData> {
-    const url = 'https://api.open-meteo.com/v1/forecast?latitude=32.7157&longitude=-117.1647&daily=sunrise,sunset,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,rain,showers,visibility,relative_humidity_2m,uv_index,precipitation&current=temperature_2m,precipitation,wind_speed_10m,wind_direction_10m,wind_gusts_10m,apparent_temperature,showers,relative_humidity_2m,rain,cloud_cover,weather_code&minutely_15=precipitation,rain&timezone=America%2FLos_Angeles&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch';
+    const url =
+        "https://api.open-meteo.com/v1/forecast?latitude=32.7157&longitude=-117.1647&current=temperature_2m&timezone=America%2FLos_Angeles&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch";
 
     const response = await fetch(url);
     const response_data: WeatherData = await response.json();
-    
+
     return response_data;
 }
 
 export default function Home() {
-    const [ weather, set_weather ] = useState<WeatherData | null>(null);
+    const [weather, set_weather] = useState<string | undefined>(undefined);
 
     useEffect(() => {
-        (async() => {
+        (async () => {
             const weather_response = await read_weather();
-            set_weather(weather_response);
+
+            const {
+                current: { temperature_2m },
+                current_units: { temperature_2m: temp_units },
+            } = weather_response;
+
+            set_weather(`${temperature_2m} ${temp_units}`);
         })();
     }, []);
 
     return (
         <div>
             <header>
-                <p><Sun /></p>
-                <p>{weather?.current?.apparent_temperature}</p>
                 <p>
-                    {new Date(Date.now()).toLocaleDateString('en-US', {
-                        month: '2-digit',
-                        day: '2-digit',
-                        year: 'numeric',
+                    <Sun />
+                </p>
+                <p>{weather}</p>
+                <p>
+                    {new Date(Date.now()).toLocaleDateString("en-US", {
+                        month: "2-digit",
+                        day: "2-digit",
+                        year: "numeric",
                     })}
                 </p>
             </header>
             <main>
-                <h1>Welcome home. :)</h1>
-                <Input type='search' placeholder="Hey, what's up again?" />
+                <h1>Welcome home Terrance. :)</h1>
+                <Input
+                    type="search"
+                    placeholder="Hey, what's up again?"
+                    onKeyDownCapture={(e) => {
+                        if (e.key === "Enter") {
+                            window.location.href = `https://duckduckgo.com/?q=${(e.target as HTMLInputElement).value}`;
+                        }
+                    }}
+                />
 
                 <div>
                     <ul>
@@ -57,7 +79,15 @@ export default function Home() {
                 </div>
             </main>
             <footer>
-                <p>Created by <a href='https://terrancecorley.com' rel='noopener noreferrer'>Terrance Corley</a></p>
+                <p>
+                    Created by{" "}
+                    <a
+                        href="https://terrancecorley.com"
+                        rel="noopener noreferrer"
+                    >
+                        Terrance Corley
+                    </a>
+                </p>
             </footer>
         </div>
     );
